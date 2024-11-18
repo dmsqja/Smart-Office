0db64039ed6245eaaef0e9f1293c3ca23a64c4fd
eunbum <bum17822@naver.com>
2024-11-18 21:03:42 +0900

    Feat: 레이아웃 구성 요소 추가 및 기존 헤더 수정, 바닥글 텍스트 수정
    - 상단 헤더를 좌측 dashboard로 변경
    - 아이콘 추가

--- front/src/components/layout/Layout.jsx
+++ front/src/components/layout/Layout.jsx
@@ -1,32 +1,36 @@
 import PropTypes from 'prop-types';
 import Header from './Header';
+import Sidebar from './Sidebar';
+import BottomNav from './BottomNav';
 import Footer from './Footer';
-import '../../styles/layout.css';
 import { useEffect, useState } from 'react';
+import '../../styles/layout.css';
 
 const Layout = ({ children }) => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    
+
     useEffect(() => {
         if (isMenuOpen) {
             document.body.style.overflow = 'hidden';
         } else {
             document.body.style.overflow = 'visible';
         }
-
         return () => {
             document.body.style.overflow = 'visible';
         };
     }, [isMenuOpen]);
 
+
     return(
         <div className="layout">
-            <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
-            <main className={`main-content ${isMenuOpen ? 'menu-open' : ''}`}>
+            <Header setIsMenuOpen={setIsMenuOpen} />
+            <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+            <main className="main-content">
                 <div className="page-container">
                     {children}
                 </div>
             </main>
+            <BottomNav />
             <Footer />
         </div>
     );
