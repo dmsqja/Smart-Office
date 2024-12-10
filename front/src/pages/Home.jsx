85fd61582a5caaec7b73829fb8548d44278a32c5
eunbum <bum17822@naver.com>
2024-12-10 20:37:32 +0900

    Feat: 홈 페이지에서 Hero 및 About 컴포넌트 제거

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,14 +1,10 @@
 // 메인 화면
-import Hero from '../components/home/Hero';
-import About from '../components/home/About';
 import Main from '../components/home/Main';
 import '../styles/pages.css'
 
 const Home = () => {
     return (
         <div className="page home-page">
-            {/* <Hero />
-      <About /> */}
             <Main />
         </div>
     );
