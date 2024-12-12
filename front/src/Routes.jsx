36cffe8a2f180f447feed4e01abc6a541af7ce0a
eunbum <bum17822@naver.com>
2024-12-12 19:59:47 +0900

    Feat: 페이지 및 컴포넌트 삭제 (AI, 연락처, 이력서, 프로젝트, 기술)

--- front/src/Routes.jsx
+++ front/src/Routes.jsx
@@ -8,13 +8,9 @@ import { CalendarProvider } from './context/CalendarContext';
 const Login = lazy(() => import('./components/login/LoginForm'));
 const PasswordChange = lazy(() => import('./components/login/PasswordChangeForm'));
 const Home = lazy(() => import('./pages/Home'));
-const Resume = lazy(() => import('./pages/Resume'));
-const Projects = lazy(() => import('./pages/Projects'));
-const Contact = lazy(() => import('./pages/Contact'));
 const Calendar = lazy(() => import ('./pages/Calendar'));
 const Messenger = lazy(() => import ('./pages/Messenger'));
-const Ai = lazy(() => import ('./pages/Ai'));
-const Hub = lazy(() => import ('./pages/File'));
+const File = lazy(() => import ('./pages/File'));
 const Meeting = lazy(() => import ('./pages/Meeting'));
 const Employee = lazy(() => import ('./pages/Employee'));
 const Document = lazy(() => import ('./pages/Document'));
@@ -32,13 +28,9 @@ const AppRoutes = () => {
             {/* Protected routes with Layout */}
             <Route element={<Layout/>}>
                 <Route path="/home" element={<Home/>}/>
-                <Route path="/resume" element={<Resume/>}/>
-                <Route path="/projects" element={<Projects/>}/>
-                <Route path="/contact" element={<Contact/>}/>
                 <Route path="/calendar" element={<Calendar/>}/>
                 <Route path="/messenger" element={<Messenger/>}/>
-                <Route path="/ai" element={<Ai/>}/>
-                <Route path="/hub" element={<Hub/>}/>
+                <Route path="/file" element={<File/>}/>
                 <Route path="/meeting" element={<Meeting/>}/>
                 <Route path="/employee" element={<Employee/>}/>
                 <Route path="/document" element={<Document/>}/>
